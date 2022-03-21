be3eb001d048fc6bef5999e7573dcd6e9c2a9951
Juan Diego Gómez Ruiz <juandieruiz6@gmail.com>
2022-03-21 20:10:00 +0100

    Bean @Primary comparado con @Qualifier

--- src/main/java/com/juandieruiz/di/qualifiers/Pajaro.java
+++ src/main/java/com/juandieruiz/di/qualifiers/Pajaro.java
@@ -4,10 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("pajarito")
+@Primary // El Qualifier hace que tome pajarito como valor, si no estuviera ese bean iría a Primary
 public class Pajaro extends Animal implements Volador {
 
     private static final Logger log = LoggerFactory.getLogger(Pajaro.class);
--- src/main/java/com/juandieruiz/di/qualifiers/Perro.java
+++ src/main/java/com/juandieruiz/di/qualifiers/Perro.java
@@ -1,9 +1,11 @@
 package com.juandieruiz.di.qualifiers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+// @Primary  Para que sea el bean por defecto, si llegase haber un qualifier daría error
 public class Perro extends Animal {
 
     public Perro(@Value("Rocky") String nombre,@Value("4")  Integer edad) {
