05ff8d57439286d423bad31af9dab84b7570d0b2
Juan Diego Gómez Ruiz <juandieruiz6@gmail.com>
2022-03-21 14:14:52 +0100

    inyection finish

--- src/main/java/com/juandieruiz/di/DependencyInyectionApplication.java
+++ src/main/java/com/juandieruiz/di/DependencyInyectionApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.juandieruiz.di.atributo.Coche;
+import com.juandieruiz.di.inyection.Coche;
 
 
 @SpringBootApplication
--- src/main/java/com/juandieruiz/di/inyection/Motor.java
+++ src/main/java/com/juandieruiz/di/inyection/Motor.java
@@ -1,4 +1,4 @@
-package com.juandieruiz.di.atributo;
+package com.juandieruiz.di.inyection;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
