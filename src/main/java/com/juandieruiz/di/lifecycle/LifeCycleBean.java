604bea29c69a4d88058634c0702163c99de5983a
Juan Diego Gómez Ruiz <juandieruiz6@gmail.com>
2022-03-24 14:21:40 +0100

    Ciclos de vida - Callback @PostConstruct y @PreDestroy

--- src/main/java/com/juandieruiz/di/lifecycle/LifeCycleBean.java
+++ src/main/java/com/juandieruiz/di/lifecycle/LifeCycleBean.java
@@ -5,13 +5,26 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
+// CICLOS DE VIDA DE UN BEAN
 @Component
 public class LifeCycleBean implements BeanNameAware {
     private static final Logger log = LoggerFactory.getLogger(LifeCycleBean.class);
 
     @Override
     public void setBeanName(String name) {
-        log.info("Bean name is {}", name);
+        log.info("Bean name Aware {}", name);
+    }
+
+    @PostConstruct
+    public void init(){
+        log.info("Bean init");
+    }
+
+    @PreDestroy
+    public void destroy(){
+        log.info("Bean Predestroy");
     }
 }
