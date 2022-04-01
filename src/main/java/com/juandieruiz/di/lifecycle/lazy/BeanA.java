59b3d7e6729df0e5c85f7247389ac0a0e1698ac7
Juan Diego Gómez Ruiz <juandieruiz6@gmail.com>
2022-04-01 14:34:06 +0200

    Bean Eager B inicializando los dos Beans(A,B)

--- src/main/java/com/juandieruiz/di/lifecycle/lazy/BeanA.java
+++ src/main/java/com/juandieruiz/di/lifecycle/lazy/BeanA.java
@@ -2,12 +2,16 @@ package com.juandieruiz.di.lifecycle.lazy;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
+@Component
 @Lazy
 public class BeanA {
+
     Logger log = LoggerFactory.getLogger(BeanB.class);
 
     @PostConstruct
--- src/main/java/com/juandieruiz/di/lifecycle/lazy/BeanB.java
+++ src/main/java/com/juandieruiz/di/lifecycle/lazy/BeanB.java
@@ -2,12 +2,18 @@ package com.juandieruiz.di.lifecycle.lazy;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
-@Lazy
+@Component
+@Lazy(false) // Eager
 public class BeanB {
+
+    @Autowired
+    private BeanA bean;
 
     Logger log = LoggerFactory.getLogger(BeanB.class);
 
